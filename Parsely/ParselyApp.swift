5771bf4b6f247f4bdddfb031a9283bcd913d6f6d
Colin Dively <colinblairdively@gmail.com>
2022-10-14 00:16:04 -0400

    inital commit

---
+++ Parsely/ParselyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParselyApp.swift
+//  Parsely
+//
+//  Created by Colin Dively on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ParselyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
